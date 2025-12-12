43ab5e83d980e1c14eff150743b1b903eb88d35f
kelgruber <80933498+kelgruber@users.noreply.github.com>
2025-12-11 22:43:54 -0600

    removed unneeded code

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import cv2
 import os
+import sys
 import streamlit as st
+
+def hello():
+    st.title("Hello")
 
 @st.cache_data
 def save_uploadedfile(filepath, uploaded_img):
@@ -49,38 +53,3 @@ def open_image(file):
         print(f"Error, could not open {file}. Program terminated.")
     return image
 
-def display_resize(image, desired_width=None, desired_height=None):
-    """
-    Function resizes image using nearest neighbor interpolation to fit within desired shape 
-    while preserving the aspect ratio.
-
-    Parameters:
-        image (numpy ndarray): array of values representing the image shape and values
-        desired_width (int): max columns wide the displayed image should be
-        desired_height (int): max rows tall the displayed image should be
-    Returns:
-        resized_image (numpy ndarray): image array resized to desired dimensions
-    """
-    current_height, current_width = image.shape[:2]
-    aspect_ratio = current_width / current_height
-
-    if desired_width is None:
-        # receved only height calculate based on the desired width
-        new_width = int(desired_height * aspect_ratio)
-        resized_image = cv2.resize(image, (new_width, desired_height), interpolation=cv2.INTER_NEAREST)
-    elif desired_height is None:
-        # receved only width calculate height based on the desired width
-        new_height = int(desired_width / aspect_ratio)
-        resized_image = cv2.resize(image, (desired_width, new_height), interpolation=cv2.INTER_NEAREST)
-    else:
-        # recieved both width and height
-        new_width = int(desired_height * aspect_ratio)
-
-        # verify new image size is within bounds if needed resize using height
-        if new_width > desired_width:
-            new_height = int(desired_width / aspect_ratio)
-            resized_image = cv2.resize(image, (desired_width, new_height), interpolation=cv2.INTER_NEAREST)
-        else:
-            resized_image = cv2.resize(image, (new_width, desired_height), interpolation=cv2.INTER_NEAREST)
-
-    return resized_image
