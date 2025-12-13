b1490030da01c93f93f3998b20d090ab0e58be47
kelgruber <80933498+kelgruber@users.noreply.github.com>
2025-12-13 01:22:28 -0600

    added segmentation page

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from pages.utils import *
 about_page = st.Page("pages/about.py", title='About')
 edge_page = st.Page("pages/edge_detect.py", title="Edge Detection")
 morph_page = st.Page("pages/morphological.py", title="Morphological Operations")
+segment_page = st.Page("pages/segment.py", title="Image Segmentation")
 
-pages = st.navigation([about_page, edge_page, morph_page])
+pages = st.navigation([about_page, edge_page, morph_page, segment_page])
 
 pages.run()
 
---
+++ pages/segment.py
@@ -0,0 +1,10 @@
+from pages.utils import *
+
+# Set page config
+st.set_page_config(page_title="Image Segmentation", page_icon=":camera:")
+
+# App Title and Give User Instructions
+st.title("Image Segmentation")
+st.markdown("""
+ Choose an Image Segmentation Algorithm from the sidebar.
+""")
